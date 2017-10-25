c15b26abe82fc71d39f5f8311cdf1cd0964a9856
25564 <cianoleary98@gmail.com>
2017-10-25 19:51:34 +1000

    Connecting the Two Services

--- ingestion/index.js
+++ ingestion/index.js
@@ -2,6 +2,7 @@
 
 
 const Twitter = require('twitter');
+const request = require('request-promise');
 
 const client = new Twitter({
   consumer_key: 'sAQgWb50UaFAWxiAyLmpjAo3O',
@@ -10,12 +11,25 @@ const client = new Twitter({
   access_token_secret: 'puTPcI2yWKtWdT9It5Sa66N2IRqqf72XlNb1I2szrW6JB',
 });
 
-const stream = client.stream('statuses/filter', { track: 'coffee' });
+const stream = client.stream('statuses/filter', { track: 'javascript, programming, computer' });
 
-stream.on('data', tweet => {
-    console.log('Recieved');
-    console.log(JSON.stringify(tweet.text));
-    console.log('\n\n')
+stream.on('data', tweetData => {
+    const RequestOptions = {
+        method: 'POST',
+        uri: 'http://localhost:3000',
+        body: {
+            tweet: tweetData.text,
+        },
+        json: true
+    };
+     
+    request(RequestOptions)
+    .then(parsedBody => {
+        console.log(parsedBody);
+    })
+    .catch(err => {
+        console.log(err);
+    });
 });
 
 stream.on('error', error => {
